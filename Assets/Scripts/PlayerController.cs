d60ccf947e9fff5c2278a7bcc2426c38b8ccfb06
DESKTOP-0BD15GP\Admin <quyettm134@gmail.com>
2025-06-17 23:08:38 +0700

    init

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 1.0f;
+
+    private PlayerControls playerControls;
+    private Vector2 movement;
+    private Rigidbody2D rb;
+
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        playerControls = new PlayerControls();
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnEnable()
+    {
+        playerControls.Enable();
+    }
+
+    private void Update()
+    {
+        PlayerInput();
+    }
+
+    private void FixedUpdate()
+    {
+        AdjustFacingDirection();
+        Move();
+    }
+
+    private void PlayerInput()
+    {
+        movement = playerControls.Movement.Move.ReadValue<Vector2>();
+
+        animator.SetFloat("horizontal", movement.x);
+        animator.SetFloat("vertical", movement.y);
+    }
+
+    private void Move()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.deltaTime);
+    }
+
+    private void AdjustFacingDirection()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(transform.position);
+
+        spriteRenderer.flipX = mousePos.x < playerScreenPoint.x;
+    }
+}
